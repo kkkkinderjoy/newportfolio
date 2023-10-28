cea876b055b86bd2e5d29c9f2320823c97313eec
홍윤정 <kkkkinderjoy@naver.com>
2023-10-28 13:45:49 +0900

    1028

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,10 +36,10 @@ function Header() {
           </ul>
           <ul className='flex dark:bg-transparent'>
                     <li className="flex items-center ml-1 dark:bg-transparent">
-                    <NavLink to="/about" className='nav text-sm lg:text-xl relative mr-1 '> About me  </NavLink> ,</li>
-                    <li className="flex items-center ml-1 dark:bg-transparent"><NavLink to="/projects" className='nav text-sm lg:text-xl relative'> Projects </NavLink></li>
-                    <button type='radio' className='text-slate-400 shadow-off p-4 rounded-[70px] w-14 h-10 font-sm  flex items-center justify-center ml-3 dark:shadow-on' onClick={()=>{dispatch(toggleTheme())}}>
-                    <input type="hidden"/>{darkMode === "light" ? "off" : "on"}
+                    <NavLink to="/about" className='nav text-sm lg:text-xl relative mr-1 '> About me</NavLink></li>
+                    <li className="flex items-center mx-5 dark:bg-transparent"><NavLink to="/projects" className='nav text-sm lg:text-xl relative'> Projects </NavLink></li>
+                    <button type='radio' className='mt-1 text-slate-400 shadow-off p-4 rounded-[70px] w-14 h-10 font-sm  flex items-center justify-center ml-3 dark:shadow-on' onClick={()=>{dispatch(toggleTheme())}}>
+                    <input type="hidden"/>{darkMode === "light" ? "dark" : "light"}
                     </button>
           </ul>
      </div>
