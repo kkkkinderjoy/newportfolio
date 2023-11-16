8b1bf8160011bfeb22b455dba9efdba051bbf812
홍윤정 <kkkkinderjoy@naver.com>
2023-11-16 16:21:06 +0900

    수정

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,7 @@ function Contact() {
         <div className="w-full h-full flex py-20">
             <div className="max-w-7xl mx-auto">
               <p className='inline-block w-[71rem] text-2xl font-semibold lg:text-3xl text-left mb-10 italic relative after:absolute after:ml-[1rem] after:w-full after:h-[1px] after:bg-[#7d7789] after:left-25 after:top-1/2 after:-translate-y-1/2'>Contact</p>
+              <div className=""></div>
               
             </div>
         </div>
